be9d200f03df75f8d1ad5c2dd40514e6765c1e0c
sablu1234 <mdsablu0000000@gmail.com>
2025-09-26 09:51:45 +0600

    up

--- wp-content/themes/harry-shop/inc/harry-woo.php
+++ wp-content/themes/harry-shop/inc/harry-woo.php
@@ -18,6 +18,15 @@ remove_action('woocommerce_after_shop_loop_item_title','woocommerce_template_loo
 remove_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_product_link_close',5);
 remove_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_add_to_cart',10);
 
+add_filter( 'woosw_button_position_archive', '__return_false' );
+add_filter( 'woosw_button_position_single', '__return_false' );
+
+add_filter( 'woosq_button_position', '__return_false' );
+
+add_filter( 'woosc_button_position_archive', '__return_false' );
+add_filter( 'woosc_button_position_single', '__return_false' );
+
+// harry add_to_cart button
 function harry_custom_add_to_cart( $args = array() ) {
     global $product;
 
@@ -100,32 +109,27 @@ function harry_product_grid(){
             </div>
 
             <div class="product__action d-flex flex-column flex-wrap">
-                <button type="button" class="product-action-btn">
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1.60361 7.98635C2.83627 11.8048 7.70625 14.8923 9.00046 15.6565C10.2991 14.8844 15.2042 11.7628 16.3973 7.98985C17.1807 5.55102 16.4536 2.46177 13.5645 1.53473C12.1648 1.08741 10.5321 1.35966 9.4049 2.22804C9.16927 2.40837 8.8422 2.41187 8.60481 2.23329C7.41084 1.33952 5.85111 1.07778 4.42941 1.53473C1.5447 2.4609 0.82023 5.55014 1.60361 7.98635ZM9.00138 17.0711C8.89236 17.0711 8.78421 17.0448 8.68574 16.9914C8.41055 16.8417 1.92808 13.2841 0.348132 8.3872C0.347252 8.3872 0.347252 8.38633 0.347252 8.38633C-0.644504 5.30321 0.459792 1.42874 4.02502 0.284605C5.69904 -0.254635 7.52342 -0.0174044 8.99874 0.909632C10.4283 0.00973263 12.3275 -0.238878 13.9681 0.284605C17.5368 1.43049 18.6446 5.30408 17.6538 8.38633C16.1248 13.2272 9.59485 16.8382 9.3179 16.9896C9.21943 17.0439 9.1104 17.0711 9.00138 17.0711Z" fill="currentColor"/>
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M14.2027 6.67485C13.8625 6.67485 13.5741 6.41486 13.546 6.07171C13.4879 5.35214 13.0044 4.74462 12.3159 4.52315C11.9686 4.4111 11.7787 4.04081 11.8904 3.69678C12.0038 3.35188 12.3722 3.16454 12.7204 3.27309C13.9187 3.65914 14.7584 4.71573 14.8613 5.96491C14.8903 6.32645 14.6204 6.64334 14.2572 6.67222C14.2388 6.67398 14.2212 6.67485 14.2027 6.67485Z" fill="currentColor"/>
-                    </svg>                                       
-                    <span class="product-action-tooltip">Add To Wishlist</span>
-                </button>
-                <button type="button" class="product-action-btn" data-bs-toggle="modal" data-bs-target="#productModal">
-                    <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.49943 5.34978C8.23592 5.34978 7.20896 6.37595 7.20896 7.63732C7.20896 8.89774 8.23592 9.92296 9.49943 9.92296C10.7629 9.92296 11.7908 8.89774 11.7908 7.63732C11.7908 6.37595 10.7629 5.34978 9.49943 5.34978M9.49941 11.3456C7.45025 11.3456 5.78394 9.68213 5.78394 7.63738C5.78394 5.59169 7.45025 3.92725 9.49941 3.92725C11.5486 3.92725 13.2158 5.59169 13.2158 7.63738C13.2158 9.68213 11.5486 11.3456 9.49941 11.3456" fill="currentColor"/>
+                <?php if ( function_exists( 'woosw_init' ) ) : ?>
+                    <diV class="product-action-btn">
+                        <?php echo do_shortcode('[woosw]'); ?>                                      
+                        <span class="product-action-tooltip"><?phP echo esc_html__('Add To Wishlist','harry'); ?></span>
+                    </diV>
+                    <?php endif; ?>
+
+                <?php if ( function_exists( 'woosq_init' ) ) : ?>
+                <div class="product-action-btn" data-bs-toggle="modal" data-bs-target="#productModal">
+                    <?php echo do_shortcode('[woosq]'); ?>
                         
-                        <path d="M1.49145 7.63683C3.25846 11.5338 6.23484 13.8507 9.50001 13.8517C12.7652 13.8507 15.7416 11.5338 17.5086 7.63683C15.7416 3.7408 12.7652 1.42386 9.50001 1.42291C6.23579 1.42386 3.25846 3.7408 1.49145 7.63683V7.63683ZM9.50173 15.2742H9.49793H9.49698C5.56775 15.2714 2.03943 12.5219 0.0577129 7.91746C-0.0192376 7.73822 -0.0192376 7.53526 0.0577129 7.35601C2.03943 2.75248 5.5687 0.00306822 9.49698 0.000223018C9.49888 -0.000725381 9.49888 -0.000725381 9.49983 0.000223018C9.50173 -0.000725381 9.50173 -0.000725381 9.50268 0.000223018C13.4319 0.00306822 16.9602 2.75248 18.942 7.35601C19.0199 7.53526 19.0199 7.73822 18.942 7.91746C16.9612 12.5219 13.4319 15.2714 9.50268 15.2742H9.50173Z" fill="currentColor"/>
-                        
-                        </svg>
-                        
-                    <span class="product-action-tooltip">Quick view</span>
-                </button>
-                <button type="button" class="product-action-btn">
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.4144 6.16828L14 3.58412L11.4144 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M1.48883 3.58386L14 3.58386" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M4.07452 8.32166L1.4889 10.9058L4.07452 13.4899" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M14 10.906H1.48883" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>                                       
-                    <span class="product-action-tooltip">Add To Compare</span>
-                </button>
+                    <span class="product-action-tooltip"><?phP echo esc_html__('Quick view','harry'); ?></span>
+                </div>
+                <?php endif; ?>
+
+                <?php if ( function_exists( 'woosc_init' ) ) : ?>
+                <div class="product-action-btn">
+                    <?php echo do_shortcode('[woosc]'); ?>                                      
+                    <span class="product-action-tooltip"><?phP echo esc_html__('Add To Compare','harry'); ?></span>
+                </div>
+                <?php endif; ?>
             </div>
             <div class="product__add transition-3">
 
